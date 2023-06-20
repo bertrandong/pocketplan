7412627a56ed11100e6c4512bd079f42248afa81
Bertrand <bertrandongchangheng@gmail.com>
2023-06-20 19:16:27 +0800

    Routed Calendar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,6 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from './Pages/Dashboard';
 import TodoPage from './Pages/TodoPage';
 import Sidebar from './Sidebar';
+import Calendar from './Pages/calendar/Calendar';
+import Analytics from './Pages/Analytics';
+import About from './Pages/About';
 import './Home.css';
 
 const Home = () => {
@@ -13,6 +16,9 @@ const Home = () => {
         <Route path="/*" element={<Dashboard />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/todopage" element={<TodoPage />} />
+        <Route path='/calendar' element={<Calendar />} />
+        <Route path='/analytics' element={<Analytics />} />
+        <Route path='/about' element={<About />} />
       </Routes>
       </Sidebar>
     </div>
