a781ddc06ec133a714c946ecad4626b880672c6b
Bertrand <bertrandongchangheng@gmail.com>
2023-07-06 18:18:14 +0800

    Updated calendar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from './Pages/Dashboard';
 import TodoPage from './Pages/TodoPage';
 import Sidebar from './Sidebar';
-import Calendar from './Pages/Newcal/Calendar';
+import ReactBigCalendar from './Pages/calendar/Calendar'
 import Analytics from './Pages/Analytics';
 import About from './Pages/About';
 import './Home.css';
@@ -16,7 +16,7 @@ const Home = () => {
         <Route path="/*" element={<Dashboard />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/todopage" element={<TodoPage />} />
-        <Route path='/calendar' element={<Calendar />} />
+        <Route path='/calendar' element={<ReactBigCalendar />} />
         <Route path='/analytics' element={<Analytics />} />
         <Route path='/about' element={<About />} />
       </Routes>
--- client/src/components/Pages/calendar/Calendar.js
+++ client/src/components/Pages/calendar/Calendar.js
@@ -1,119 +1,119 @@
-import React, { useState, useRef, useEffect } from "react";
-import {
-  DayPilot,
-  DayPilotCalendar,
-  DayPilotNavigator,
-} from "@daypilot/daypilot-lite-react";
-import "./CalendarStyles.css";
+import React, { Component } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { v4 } from 'uuid';
 
-const styles = {
-  wrap: {
-    display: "flex",
-  },
-  left: {
-    marginRight: "10px",
-  },
-  main: {
-    flexGrow: "1",
-  },
-};
+import axios from 'axios'
 
-const Calendar = () => {
-  const calendarRef = useRef();
-  const [calendarConfig, setCalendarConfig] = useState({
-    viewType: "Week",
-    durationBarVisible: false,
-    timeRangeSelectedHandling: "Enabled",
-    onTimeRangeSelected: async (args) => {
-      const dp = calendarRef.current.control;
-      const modal = await DayPilot.Modal.prompt(
-        "Create a new event:",
-        "Event 1"
-      );
-      dp.clearSelection();
-      if (!modal.result) {
-        return;
-      }
-      dp.events.add({
-        start: args.start,
-        end: args.end,
-        id: DayPilot.guid(),
-        text: modal.result,
+
+const localizer = momentLocalizer(moment);
+
+class cal extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      cal_events: [
+        //State is updated via componentDidMount
+      ],
+    }
+
+  }
+
+  convertDate = (date) => {
+    return moment.utc(date).toDate()
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:4000/api/calendar')
+      .then(response => {
+        console.log(response.data);
+        let appointments = response.data;
+        
+        for (let i = 0; i < appointments.length; i++) {
+          appointments[i].start = this.convertDate(appointments[i].start)
+          appointments[i].end = this.convertDate(appointments[i].end)
+        }
+
+        this.setState({
+          cal_events:appointments
+        })
+  
+      })
+      .catch(function (error) {
+        console.log(error);
       });
-    },
-    eventDeleteHandling: "Update",
-    onEventClick: async (args) => {
-      const dp = calendarRef.current.control;
-      const modal = await DayPilot.Modal.prompt(
-        "Update event text:",
-        args.e.text()
-      );
-      if (!modal.result) {
-        return;
-      }
-      const e = args.e;
-      e.data.text = modal.result;
-      dp.events.update(e);
-    },
-  });
+  }
 
-  useEffect(() => {
-    const events = [
-      {
-        id: 1,
-        text: "Event 1",
-        start: "2023-06-20T10:30:00",
-        end: "2023-06-20T13:00:00",
-      },
-      {
-        id: 2,
-        text: "Event 2",
-        start: "2023-06-21T09:30:00",
-        end: "2023-06-21T11:30:00",
-        backColor: "#6aa84f",
-      },
-      {
-        id: 3,
-        text: "Event 3",
-        start: "2023-06-21T12:00:00",
-        end: "2023-06-21T15:00:00",
-        backColor: "#f1c232",
-      },
-      {
-        id: 4,
-        text: "Event 4",
-        start: "2023-06-19T11:30:00",
-        end: "2023-06-19T14:30:00",
-        backColor: "#cc4125",
-      },
-    ];
+  render() {
+    const { cal_events } = this.state
+    //const DnDCalendar = withDragAndDrop(Calendar)
 
-    const startDate = "2023-06-20";
-
-    calendarRef.current.control.update({ startDate, events });
-  }, []);
-
-  return (
-    <div style={styles.wrap}>
-      <div style={styles.left}>
-        <DayPilotNavigator
-          selectMode={"Week"}
-          showMonths={3}
-          skipMonths={3}
-          startDate={"2023-06-20"}
-          selectionDay={"2023-06-20"}
-          onTimeRangeSelected={(args) => {
-            calendarRef.current.control.update({
-              startDate: args.day,
-            });
-          }}
-        />
+    return (
+      <div className="Container">
+        <div style={{ height: 700 }}>
+          <Calendar 
+            selectable
+            popup={true}
+            localizer={localizer}
+            events={cal_events}
+            step={30}
+            style={{ height: "100vh" }}
+            defaultView='month'
+            views={['month','week','day','agenda']}
+            defaultDate={new Date()}
+            onSelectSlot={this.handleSelect}
+            onSelectEvent={this.handleSelectEvent}
+          />
+        </div>
       </div>
-      <div style={styles.main}>
-        <DayPilotCalendar {...calendarConfig} ref={calendarRef} />
-      </div>
-    </div>
-  );
-};
+    );
+  }
 
-export default Calendar;
+  handleSelect = async ({ start, end }) => {
+    const id = v4();
+    console.log(start);
+    console.log(end);
+    const title = window.prompt("New Event name");
+
+    if (title) {
+      const event = {_id: id, title: title, start: start, end: end}
+
+      this.setState(prevState => ({
+        cal_events: [...prevState.cal_events, event]
+      }))
+  
+      const response = await fetch('/api/calendar/createEvent', {
+        method: 'POST',
+        body: JSON.stringify(event),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const json = await response.json()
+  
+    }
+  }
+
+  handleSelectEvent = async (eventInfo) => {
+    if(window.confirm(`Are you sure you want to delete the event '${eventInfo.title}'`)) {
+      const id = eventInfo._id
+  
+      
+      this.setState(prevState => {
+        let arr = [...prevState.cal_events]
+        return {cal_events: arr.filter(event => event._id !== id)}
+      })
+
+      const response = await fetch('api/calendar/' + id, {
+        method: 'DELETE'
+      })
+      const json = await response.json();
+    }
+  }
+}
+
+export default cal;
