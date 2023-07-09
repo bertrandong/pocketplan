e6572933c9d87025eba956d6e95f727b813cfc31
Bertrand <bertrandongchangheng@gmail.com>
2023-07-09 16:23:15 +0800

    Created user login database

--- client/src/utils/resource.js
+++ client/src/utils/resource.js
@@ -2,7 +2,7 @@ import { toast } from "react-toastify";
 
 export async function handleLogin(username, password, navigate) {
 	try {
-		const request = await fetch("http://localhost:4000/login", {
+		const request = await fetch("/user/login", {
 			method: "POST",
 			body: JSON.stringify({
 				username,
@@ -14,21 +14,25 @@ export async function handleLogin(username, password, navigate) {
 			},
 		});
 		const data = await request.json();
+
 		if (data.error_message) {
 			toast.error(data.error_message);
 		} else {
 			toast.success(data.message);
-			localStorage.setItem("_id", data.data._id);
-			localStorage.setItem("_myEmail", data.data._email);
+            console.log('Got the token: ', data.data)
+            localStorage.setItem('token', data.data)
+			//localStorage.setItem("_id", data.data._id);
+			//localStorage.setItem("_myEmail", data.data._email);
 			navigate("/home");
 		}
 	} catch (err) {
 		console.error(err);
+        toast.error("Login failed");
 	}
 }
 export async function handleRegister(email, username, password, navigate) {
     try {
-        const request = await fetch("http://localhost:4000/register", {
+        const request = await fetch("/user/register", {
             method: "POST",
             body: JSON.stringify({
                 email,
@@ -53,4 +57,25 @@ export async function handleRegister(email, username, password, navigate) {
     }
 };
 
+/*export async function handleRegister(email, username, password, navigate) {
+
+        const request = await fetch("/api/register'", {
+            method: "POST",
+            body: JSON.stringify({
+                email,
+                username,
+                password,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await request.json();
+        if (data.status === 'ok') {
+            window.prompt('Success')
+            navigate('/')
+        } else {
+            window.prompt(data.error)
+        }
+};*/
 
