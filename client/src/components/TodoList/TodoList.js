a3141d5e7abd90883f446761421756211268259d
Bertrand <bertrandongchangheng@gmail.com>
2023-07-23 01:40:43 +0800

    Bug fix

--- client/src/components/TodoList/TodoList.js
+++ client/src/components/TodoList/TodoList.js
@@ -20,7 +20,7 @@ const TodoList = () => {
     useEffect(() => {
         const fetchData = async () => {
             const token = localStorage.getItem('token')
-            const response = await fetch('api/todolist/getTasks', {
+            const response = await fetch('https://pocketplanner-api.vercel.app/api/todolist/getTasks', {
                 method: 'POST',
                 body: JSON.stringify({token: token}),
                 headers: {
@@ -39,7 +39,7 @@ const TodoList = () => {
 
     const deleteTask = async (taskObj) => {
         const id = taskObj._id
-        const response = await fetch('api/todolist/' + id, {
+        const response = await fetch('https://pocketplanner-api.vercel.app/api/todolist/' + id, {
             method: 'DELETE'
         })
         const json = await response.json();
@@ -78,7 +78,7 @@ const TodoList = () => {
         try {
             const id = v4()
             const token = localStorage.getItem('token')
-            const response = await fetch('api/todolist/createTask', {
+            const response = await fetch('https://pocketplanner-api.vercel.app/api/todolist/createTask', {
                 method: 'POST',
                 body: JSON.stringify({taskName: taskObj['Name'], description: taskObj['Description'], date: taskObj['Duedate'], token: token}),
                 headers: {
