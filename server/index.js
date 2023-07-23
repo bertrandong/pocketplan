3a39588a1bc64976a4ed6d13a4ac33278fad699e
Bertrand <bertrandongchangheng@gmail.com>
2023-07-23 11:03:36 +0800

    Server change

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
 const bodyParser = require('body-parser')
-const PORT = 4000;
+//const PORT = 4000;
 
 app.use(cors());
 app.use(bodyParser.json())
@@ -17,7 +17,7 @@ app.use("/api/todolist", require('./Routes/TodolistRoutes'))
 
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
-        app.listen(PORT, () => {
+        app.listen(process.env.PORT, () => {
             console.log(`Server listening on ${PORT}`);
         });
     })
