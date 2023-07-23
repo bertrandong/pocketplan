8f0606febe8f8412ac927ad5a19ed3ed09ad2dfe
Bertrand <bertrandongchangheng@gmail.com>
2023-07-23 15:13:48 +0800

    Dnd Calendar

--- server/Controllers/CalendarController.js
+++ server/Controllers/CalendarController.js
@@ -68,19 +68,12 @@ const deleteAll = async (req, res) => {
 const updateEvent = async (req, res) => {
     const { id } = req.params
 
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({error: 'No such event'})
+    try {
+        const event = await Event.findOneAndUpdate({_id: id}, req.body, {new: true})
+        res.status(200).json(event)
+    } catch (error) {
+        res.status(400).json({error: error.message})
     }
-
-    const event = await Event.findOneAndUpdate({_id: id}, {
-        ...req.body
-    })
-
-    if (!event) {
-        return res.status(404).json({error: 'No such event'})
-    }
-
-    res.status(200).json(event)
 }
 
 const uploadNewEvents = async (req, res) => {
