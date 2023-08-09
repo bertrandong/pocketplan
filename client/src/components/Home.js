5ab0d1e76681959aa710bbc86d80e789cada940e
Bertrand <bertrandongchangheng@gmail.com>
2023-08-09 21:17:54 +0800

    Fixed logout

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Login from "./Login";
 import Dashboard from './Pages/Dashboard';
 import TodoPage from './Pages/TodoPage';
 import Sidebar from './Sidebar';
@@ -9,20 +10,36 @@ import About from './Pages/About';
 import './Home.css';
 
 const Home = () => {
-  return (
-    <div>
-      <Sidebar>
-      <Routes>
-        <Route path="/*" element={<Dashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/todopage" element={<TodoPage />} />
-        <Route path='/calendar' element={<ReactBigCalendar />} />
-        <Route path='/analytics' element={<Analytics />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-      </Sidebar>
-    </div>
-  );
+  const isLoggedIn = localStorage.getItem('token')
+  if (isLoggedIn) {
+    return (
+      <div>
+          <Sidebar>
+          <Routes>
+            <Route path="/*" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/todopage" element={<TodoPage />} />
+            <Route path='/calendar' element={<ReactBigCalendar />} />
+            <Route path='/analytics' element={<Analytics />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+          </Sidebar>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+          <Routes>
+            <Route path="/*" element={<Login />} />
+            <Route path="/dashboard" element={<Login />} />
+            <Route path="/todopage" element={<Login />} />
+            <Route path='/calendar' element={<Login />} />
+            <Route path='/analytics' element={<Login />} />
+            <Route path='/about' element={<Login />} />
+          </Routes>
+      </div>
+    );
+  }
 };
 
 export default Home;
--- client/src/components/Pages/Analytics.js
+++ client/src/components/Pages/Analytics.js
@@ -92,9 +92,6 @@ const Analytics = () => {
 
     return (
         <main style={{'padding': '1%'}}>
-            <head>
-                <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto&display=swap" rel="stylesheet" />
-            </head>
             <h1>Track today's progress:</h1>
             <p className='specialp' style={{'textAlign': 'center', 'padding-top':'1%', 'fontWeight': '800'}}>{intro}</p>
             <button className='enterinputsbtn' onClick={() => setModal(true)}>Enter inputs</button>
